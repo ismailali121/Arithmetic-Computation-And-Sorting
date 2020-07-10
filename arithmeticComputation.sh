a31b834c9a938ce1bb682f9254d5754b0f03d5f0
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-10 12:20:06 +0530

    Added program to take three inputs

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,1 +1,4 @@
-echo "Program to do different arithmetic computaion and store results"
+#!/bin/bash -x
+read -p "Enter first value : " a
+read -p "Enter second value : " b
+read -p "Enter third value : " c
